e6619c1b17ccb5c9f1385406b615ffe231f47646
Heikman <h.burau@hzdr.de>
2017-01-18 15:30:19 +0100

    update deepForeach

--- deepForeach.hpp
+++ deepForeach.hpp
@@ -6,11 +6,11 @@
 namespace detail
 {
 
-template<typename Iter,
+template<typename Iterator,
          typename Functor>
 void deepForeach(
-                 const Iter& begin,
-                 const Iter& end,
+                 const Iterator& begin,
+                 const Iterator& end,
                  const Functor& functor,
                  std::true_type)
 {
@@ -21,36 +21,36 @@ void deepForeach(
         deepForeach(iterPair.first,
                     iterPair.second,
                     functor,
-                    is_iteratorPair< typename std::iterator_traits<Iter>::value_type >());
+                    is_iteratorPair< typename std::iterator_traits<Iterator>::value_type >());
         iter++;
     }
 }
 
 
-/* hasChild = false */
-template<typename Iter,
+template<typename Iterator,
          typename Functor>
 void deepForeach(
-                 const Iter& begin,
-                 const Iter& end,
+                 const Iterator& begin,
+                 const Iterator& end,
                  const Functor& functor,
                  std::false_type)
 {
     std::for_each(begin, end, functor);
 }
 
+
 } // namespace detail
 
 
-template<typename Iter,
+template<typename Iterator,
          typename Functor>
-void deepForeach(const Iter& begin,
-                 const Iter& end,
+void deepForeach(const Iterator& begin,
+                 const Iterator& end,
                  const Functor& functor)
 {
     detail::deepForeach(
         begin,
         end,
         functor,
-        is_iteratorPair< typename std::iterator_traits<Iter>::value_type >());
+        is_iteratorPair< typename std::iterator_traits<Iterator>::value_type >());
 }
